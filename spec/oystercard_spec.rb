c4a11e508fb14d4a54190b49a66379b5115d4efe
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-01-18 17:43:14 +0000

    completed #7: money can be deducted from the card, all tests passing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,7 @@ describe Oystercard do
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
-    it 'can top us the balance' do
+    it 'can top up the balance' do
       expect{ subject.top_up 1}.to change{ subject.balance }.by 1
     end
 
@@ -19,6 +19,15 @@ describe Oystercard do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
       expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+  end
+  
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deducts an amount from the blance' do
+      subject.top_up(20)
+      expect{ subject.deduct 3}.to change{ subject.balance }.by -3
     end
   end
 end
