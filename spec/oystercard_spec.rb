69dc6a574d5b7317620f4ec8a1d1ff7be3511b00
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-01-18 16:25:44 +0000

    completed #5: topping up card and tests passing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+ 
+  describe '#balance' do 
     it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top us the balance' do
+      expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+    end
   end
 end
 
